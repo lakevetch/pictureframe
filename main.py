8f7066e64b5c8699fa66cb82d9b639d3e1c3ccd7
Jack <jjacksanders@gmail.com>
2022-12-21 08:39:28 +0000

    Final version, the stubs of some error handling available for more work if I choose

--- main.py
+++ main.py
@@ -4,294 +4,214 @@ from tkinter import *
 import random as r
 from PIL import Image, ImageTk
 from screeninfo import get_monitors
+import pickle
+import valid as v
 
-import valid
-
-#import pickle
-#import time
-
-PATH = "C:\\Users\\Jack\\Desktop\\photos"
+PATH = "/home/picture/Pictures"
 LOOPTIME = 30
 
+class Slideshow(Tk):
+    __done = []
+    __photo = ""
 
-#class Backdrop:
-#    def __init__(self, widths, heights):
-#        self.root = Tk()
-#        self.root.configure(bg="black")
-#        display = ImageTk.PhotoImage(photo)
-#        label = tkinter.Label(image=display)
-#        label.image = display
-#        label.pack()
-        # self.root.attributes("-fullscreen", True)
-#        self.root.after((LOOPTIME + 1) * 1000, lambda: self.root.destroy())
-        #    skip_button = Button(root, text="Skip", command=root.destroy)
-        #    skip_button.pack(side=RIGHT)
-#        dimensions = str(widths[0]) + "x" + str(heights[0])
-#        self.root.geometry(dimensions)
-#        self.display_loop(photo, widths, heights)
-#        self.root.mainloop()
-
-#    def display_loop(self, photo, widths, heights):
-#        done = []
-#        end = time.time()
-#        while True:
-#            if time.time() > end:
-#                photos = load_avail_photos()
-#                remove_done(photos, done)
-#                if len(photos) == 0:
-#                    photos = load_avail_photos()
-#                    done.clear()
-#                photo = select_photo(photos, done)
-#                photo = prep_photo(photo, widths, heights)
-#                end = delay()
-#                display = Display(photo, widths, heights, self)
-
-
-
-class Display:
-    __continue = True
-    __duration = 0
-
-    def __init__(self, photo, widths, heights, looptime):
-        self.__duration = looptime * 1000
-        self.root = Tk()
-        self.root.configure(bg="black")
-        self.root.after(self.__duration, lambda: self.root.destroy())
-        button = Button(self.root, text="STOP", bg="black", fg="white", command=self.quit_show)
-        skip = Button(self.root, text="SKIP PHOTO", bg="black", fg="white", command=self.root.destroy)
-        display = ImageTk.PhotoImage(photo)
-        label = tkinter.Label(image=display)
-        label.image = display
-        label.pack(anchor="n")
-        button.pack(anchor="s")
-        skip.pack(anchor="e")
-        self.root.attributes("-fullscreen", True)
-        #    skip_button = Button(root, text="Skip", command=root.destroy)
-        #    skip_button.pack(side=RIGHT)
+    def __init__(self, photo_list, looptime, widths, heights):
+        Tk.__init__(self)
+        self.configure(bg="black")
+        self.__delay = looptime * 1000
+        self.__photo_list = photo_list
+        self.__widths = widths
+        self.__heights = heights
+        self.__display = tkinter.Label(self, bg="black", image=None)
+        self.__display.pack(anchor="n")
+        self.__quit = tkinter.Button(self, bg="black", fg="white", text="STOP", command=self.quit_show)
+        self.__quit.pack(anchor="s")
         dimensions = str(widths[0]) + "x" + str(heights[0])
-        self.root.geometry(dimensions)
-        self.root.mainloop()
-
+        self.geometry(dimensions)
+        self.attributes("-fullscreen", True)
+    
+    def prep_photo(self, photo):
+        """
+        Readies the selected photo for display and stores it as a field of the
+        Slideshow tkinter window object.
+        :param photo: Image class object from PIL
+        :return: None
+        """
+        img = self.resize_photo(photo)
+        img = ImageTk.PhotoImage(img)
+        self.__photo = img
+    
     def quit_show(self):
-        self.root.destroy()
-        self.__continue = False
-
-    def quit_query(self):
-        return self.__continue
-
+        """
+        Quits show by destroying the tkinter window. Program goes to menu.
+        :return: None
+        """
+        self.destroy()
+        
+    def resize_photo(self, photo):
+        """
+        Uses attributes of the screen being used to resize image files to
+        nearly fill the screen.
+        :param photo: Image class object from PIL to be resized
+        :return: resized Image class object
+        """
+        if photo.size[0] > photo.size[1]:
+            factor_w = (self.__widths[0] - self.__widths[0] / 30) / photo.size[0]
+            factor_h = (self.__heights[0] - self.__widths[0] / 30) / photo.size[1]
+            if factor_h > factor_w:
+                factor = factor_w
+            else:
+                factor = factor_h
+        else:
+            factor = (self.__heights[0] - self.__heights[0] / 30) / photo.size[1]
+        resized = photo.resize((int((photo.size[0] * factor) // 1), int((photo.size[1] * factor) // 1)))
+        return resized
+        
+    def run(self):
+        """
+        Command to start the tkinter application window.
+        :return: None
+        """
+        self.mainloop()
+        
+    def select_photo(self):
+        """
+        Algorithm to randomly select from the list of photos and
+        maintain a list of completed selections in order to never double
+        until all photos have been shown.
+        :return: Image class object chosen from list of available objects
+        """
+        index = 0
+        index = r.randrange(len(self.__photo_list))
+        if 0 < len(self.__done) < len(self.__photo_list):
+            while index in self.__done:
+                index = r.randrange(len(self.__photo_list))
+            self.__done.append(index)
+        elif len(self.__done) == len(self.__photo_list):
+            self.__done.clear()
+            self.__done.append(index)
+        else:
+            self.__done.append(index)
+        photo = self.__photo_list[index]
+        return photo
+    
+    def show_slide(self):
+        """
+        Main control for slideshow. Calls all preparatory functions, prepares
+        and saves an image, displays the image, and sets itself to run again
+        after the preset delay period. Also creates a skip button to skip to
+        the next run of the function.
+        :return: None
+        """
+        photo = self.select_photo()
+        self.prep_photo(photo)
+        img = self.__photo
+        self.__display.config(image=img, bg="black")
+        self.after(self.__delay, self.show_slide)
+        self.__skip = tkinter.Button(self, bg="black", fg="white", text="SKIP", command=self.show_slide)
+        self.__skip.pack(anchor="e")    
+        
 
 def main():
-    photos = []
-    done = []
     widths = []
     heights = []
-    path = PATH
-    looptime = LOOPTIME
     screen_size(widths, heights)
+    path = load_option("path")
+    looptime = load_option("looptime")
+    runs = 0
     t_main = True
     while t_main:
-        choice = main_menu()
-    #    end = time.time()
-    #    backdrop = Backdrop(widths, heights)
-    #    backdrop = create_backdrop()
+        if runs == 0:
+            choice = 1
+        else:
+            choice = main_menu()
         if choice == 1:
-            t_one = True
-            while t_one:
-        #        if time.time() > end:
-                photos = load_avail_photos(path)
-                remove_done(photos, done)
-                if len(photos) == 0:
-                    photos = load_avail_photos(path)
-                    done.clear()
-                photo = select_photo(photos, done, path)
-                if type(photo) == type(""):
-                    path = photo
-                    photos = load_avail_photos(path)
-                    done.clear()
-                    photo = select_photo(photos, done, path)
-                photo = prep_photo(photo, widths, heights)
-        #            end = delay()
-                display = Display(photo, widths, heights, looptime)
-                t_one = display.quit_query()
-        if choice == 2:
+            filename_list = load_avail_photos(path)
+            photo_list = open_photos(filename_list, path)
+            #if type(photo_list) == type("")
+            show = Slideshow(photo_list, looptime, widths, heights)
+            show.show_slide()
+            show.run()
+        elif choice == 2:
             looptime = change_freq()
-        if choice == 3:
-            cont = ""
-            cont = valid.get_y_or_n("Change path to image folder? (y/n): ")
-            if cont == "y":
+            save_option(looptime, "looptime")
+        elif choice == 3:
+            proceed = v.get_y_or_n("Change the path to the image folder? (y/n): ")
+            if proceed == y:
                 path = change_path()
-        if choice == 4:
+                save_option(path, "path")
+        elif choice == 4:
             t_main = False
-#    if choice == 2:
+        runs += 1
 
-#        float_photo(photo, backdrop)
+
+def change_freq():
+    """
+    Takes and validates user input of a new time for photos to display
+    before the next photo displays.
+    :return: integer, new number of seconds for photos to display
+    """
+    frequency = 0
+    while frequency < 1:
+        frequency = v.get_integer("Please enter a number of seconds: ")
+        if frequency < 1:
+            print("Must enter a positive non-zero number of seconds.")
+    print(f"{frequency} seconds entered.")
+    print()
+    return frequency
+
+    
+def change_path():
+    """
+    Takes user input of a new path to an image folder on the local disk.
+    :return: string, the path to the image folder
+    """
+    path = ""
+    path = v.get_string("Enter path to image folder: ")
+    return path
 
 
 def load_avail_photos(path):
+    """
+    Attempts to read the directory located at the path given and list all
+    files therein. If it fails it prompts the user for a new path and tries
+    again until a valid path is given.
+    :param path: string, path to directory where the image files are held
+    :return: list of available filenames in directory at end of path
+    """
     photos = []
-#    try:
-#        photos = pickle.load(open("list", "rb"))
-#    except:
-#        photos = os.listdir(PATH)
-#    if len(photos) == 0:
     try:
         photos = os.listdir(path)
         return photos
     except:
         print("Invalid path.")
         path = change_path()
-        load_avail_photos(path)
-        return path
+        photos = load_avail_photos(path)
+        return photos
+    
 
-
-def select_photo(photos, done, path):
+def load_option(name):
+    """
+    Looks for and attempts to load a previous user setting. Falls back on
+    defaults if no save file is found.
+    :param name: string, name of the file to look for
+    :return: object, the saved option
+    """
     try:
-        index = 0
-        index = r.randrange(len(photos))
-        photo = ""
-        photo = Image.open(path + "\\" + photos[index])
-    #    pickle.dump(photo, open("most_recent", "wb"))
-        done.append(photos[index])
-        return photo
+        option = pickle.load(open(name, "rb"))
+        return option
     except:
-        print()
-        print("Hmm, I've run into a problem.")
-        print("Either the path to your image folder is incorrect,")
-        print("or I can't read one of the files in the folder.")
-        print("Please check that you gave the correct folder. Current path:")
-        print(f"{path}")
-        print("If that is correct, then check the folder for any non-image files\n"
-              "and remove them.")
-        print()
-        print("Then either:")
-        print("1: Enter a new path and I'll try again")
-        print("2: Just have me try it again")
-        choice = ""
-        choice = valid.get_integer("Your choice: ")
-        if choice == 1:
-            path = change_path()
-            return path
-        if choice == 2:
-            photo = select_photo(photos, done, path)
-            return photo
-
-
-def prep_photo(photo, widths, heights):
-    if photo.size[0] > photo.size[1]:
-        factor_w = (widths[0] - widths[0] / 30) / photo.size[0]
-        factor_h = (heights[0] - widths[0] / 30) / photo.size[1]
-        if factor_h > factor_w:
-            factor = factor_w
-        else:
-            factor = factor_h
-    else:
-        factor = (heights[0] - heights[0] / 30) / photo.size[1]
-    resized = photo.resize((int((photo.size[0] * factor) // 1), int((photo.size[1] * factor) // 1)))
-#        if resized.size[0] > 1000 or resized.size[1] > 563:
-#            diff_w = resized.size[0] - 1000
-#            diff_h = resized.size[1] - 563
-#            if diff_w > diff_h:
-#                factor = resized.size[0] / 1000
-#            else:
-#                factor = resized.size[1] / 563
-#            resized = resized.resize((int((resized.size[0] * factor) // 1), int((resized.size[1] * factor) // 1)))
-    return resized
-
-
-#def prep_photos(photos, widths, heights):
-#    resized = []
-#    for photo in photos:
-#        if photo.size[0] > photo.size[1]:
-#            factor_w = widths[0] / photo.size[0]
-#            factor_h = heights[0] / photo.size[1]
-#            if factor_h > factor_w:
-#                factor = factor_w
-#            else:
-#                factor = factor_h
-#        else:
-#            factor = heights[0] / photo.size[1]
-#        new = photo.resize((int((photo.size[0] * factor) // 1), int((photo.size[1] * factor) // 1)))
-#        resized.append(new)
-#        if resized.size[0] > 1000 or resized.size[1] > 563:
-#            diff_w = resized.size[0] - 1000
-#            diff_h = resized.size[1] - 563
-#            if diff_w > diff_h:
-#                factor = resized.size[0] / 1000
-#            else:
-#                factor = resized.size[1] / 563
-#            resized = resized.resize((int((resized.size[0] * factor) // 1), int((resized.size[1] * factor) // 1)))
-#    return resized
-
-
-#def display_photo(photo, widths, heights):
-#    dimensions = ""
-#    root = Tk()
-#    root.configure(bg="black")
-#    display = ImageTk.PhotoImage(photo)
-#    label = tkinter.Label(image=display)
-#    label.image = display
-#    label.pack()
-#    root.attributes("-fullscreen", True)
-#    root.after((LOOPTIME + 1) * 1000, lambda: root.destroy())
-#    skip_button = Button(root, text="Skip", command=root.destroy)
-#    skip_button.pack(side=RIGHT)
-#    dimensions = str(widths[0]) + "x" + str(heights[0])
-#    root.geometry(dimensions)
-#    root.mainloop()
-
-
-def screen_size(widths, heights):
-    for monitor in get_monitors():
-        width = monitor.width
-        height = monitor.height
-        widths.append(width)
-        heights.append(height)
-
-#def end_loop():
-#    return False
-
-#def float_photo(photo, backdrop):
-#    root = Toplevel(backdrop)
-#    root.configure(bg="black")
-#    display = ImageTk.PhotoImage(photo)
-#    label = tkinter.Label(image=display)
-#    label.image = display
-#    label.pack()
-#    root.attributes("-fullscreen", True)
-#    root.after(LOOPTIME * 1000, lambda: root.destroy())
-#    skip_button = Button(root, text="Skip", command=root.destroy)
-#    skip_button.pack(side=RIGHT)
-#    root.geometry("1000x563")
-#    root.mainloop()
-
-
-
-#def save_state(photos, photo):
-#    pickle.dump(photos, open("list", "wb"))
-#    try:
-#        previous = pickle.load(open("most_recent", "rb"))
-#        previous.Image.close()
-#    except:
-#        print("Not closed")
-#        return
-
-
-#def delay():
-#    end = time.time() + LOOPTIME
-#    return end
-
-def remove_done(photos, done):
-    for name in done:
-        index = photos.index(name)
-        photos.pop(index)
-
-#def create_backdrop():
-#    backdrop = Tk()
-#    backdrop.configure(bg="black")
-#    backdrop.geometry("1000x563")
-#    return backdrop
-
+        if name == "path":
+            return PATH
+        elif name == "looptime":
+            return LOOPTIME
+        
+        
 def main_menu():
+    """
+    Prints a menu of options and prompts the user to choose one, validating
+    their choice.
+    :return: integer, the user's chosen option
+    """
     print()
     print("1: Start the slideshow!")
     print("2: Change cycle frequency (default 30 seconds)")
@@ -300,25 +220,73 @@ def main_menu():
     print()
     choice = 0
     while not choice in range(1, 5):
-        choice = valid.get_integer("Please enter a menu option: ")
+        choice = v.get_integer("Please enter a menu option: ")
     return choice
 
 
-def change_freq():
-    frequency = 0
-    while frequency < 1:
-        frequency = valid.get_integer("Please enter a number of seconds: ")
-        if frequency < 1:
-            print("Must enter a positive non-zero number of seconds.")
-    print(f"{frequency} seconds entered.")
-    print()
-    return frequency
+def open_photos(filename_list, path):
+    """
+    Takes the list of filenames at path and populates a new list of PIL Image
+    objects for each filename in list.
+    :param filename_list: list of string filenames from the directory
+    :param path: string, path to the directory
+    :return: list of Image objects
+    """
+    photo_list = []
+    try:
+        for filename in filename_list:
+            photo = Image.open(path + "/" + filename)
+            photo_list.append(photo)
+        return photo_list
+    except:
+        print("please update path")
+#        print()
+#        print("Hmm, I've run into a problem.")
+#        print("Either the path to your image folder is incorrect,")
+#        print("or I can't read one of the files in the folder.")
+#        print("Please check that you gave the correct folder. Current path:")
+#        print(f"{path}")
+#        print("If that is correct, then check the folder for any non-image\n"
+#              "files and remove them.")
+#        print()
+#        print("Then either:")
+#        print("1: Enter a new path and I'll try again")
+#        print("2: Just have me try it again")
+#        choice = ""
+#        choice = v.get_integer("Your choice: ")
+#        if choice == 1:
+#            path = change_path()
+#            return path
+#        if choice == 2:
+#            photo = select_photo(photos, done, path)
+#            return photo
 
 
-def change_path():
-    path = ""
-    path = valid.get_string("Enter path to image folder: ")
-    return path
+def save_option(option, name):
+    """
+    Saves a user-selected option for future runs of the program by writing
+    a serialized version of the object denoting the option to a file.
+    :param option: object, the option to be saved
+    :param name: string, the name of the option to be saved, becomes the name
+    of the file
+    :return: None
+    """
+    pickle.dump(option, open(name, "wb"))
+
+
+def screen_size(widths, heights):
+    """
+    Uses a module to access and return the pixel dimensions of the screen the
+    computer is displaying to.
+    :param widths: empty list to append width to
+    :param heights: empty list to append height to
+    :return: None
+    """
+    for monitor in get_monitors():
+        width = monitor.width
+        height = monitor.height
+        widths.append(width)
+        heights.append(height)
 
 
 main()
